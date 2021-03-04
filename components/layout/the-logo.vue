42480d890d6798721068a76c115ec0f6c0ad4e98
miguelbui <miguelbui.dev@gmail.com>
2021-03-05 01:28:05 +0700

    :+1: Implement layout header component

---
+++ components/layout/the-logo.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 class="the-logo">
+    <nuxt-link to="/" class="link">
+      <icon-logo class="icon"></icon-logo>
+    </nuxt-link>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'TheLogo',
+}
+</script>
+
+<style lang="postcss">
+.the-logo {
+  @apply p-2 py-4 flex justify-center items-center;
+}
+.the-logo > .link {
+  @apply p-1 inline-block bg-primary;
+}
+.the-logo > .link > .icon {
+  @apply w-5 h-5 transform rotate-90 text-black;
+}
+</style>

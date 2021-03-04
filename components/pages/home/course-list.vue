3a7cba8f652cf8e8d3a54bb2ed16c7f73122d286
miguelbui <miguelbui.dev@gmail.com>
2021-03-05 03:46:34 +0700

    :+1: Implement course layout

--- components/pages/home/course-list.vue
+++ components/pages/home/course-list.vue
@@ -5,7 +5,13 @@
       <span class="sortby"> - Sorted by most popular</span>
     </div>
     <div class="items">
-      <home-course-item class="item"></home-course-item>
+      <div
+        v-for="(course, index) in courses"
+        :key="index + '_course'"
+        class="item"
+      >
+        <pages-home-course-item :item="course"></pages-home-course-item>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +19,72 @@
 <script>
 export default {
   name: 'HomeCourseList',
+  data() {
+    return {
+      courses: [
+        {
+          id: 1,
+          courseName: 'React: Getting Started',
+          isPopular: true,
+          isNew: false,
+          authorName: 'Samuel Eto',
+          publishedDate: 'Oct 6, 2020',
+          level: 'Beginner',
+          numOfRates: 3.5,
+          score: 3587,
+          courseLength: '9h2m',
+        },
+        {
+          id: 2,
+          courseName: 'Javascript: Getting Started',
+          isPopular: false,
+          isNew: false,
+          authorName: 'Romeo Lukaku',
+          publishedDate: 'Jan 11, 2021',
+          level: 'Beginner',
+          numOfRates: 4.5,
+          score: 196,
+          courseLength: '4h2m',
+        },
+        {
+          id: 3,
+          courseName: 'I dont know JS',
+          isPopular: true,
+          isNew: true,
+          authorName: 'Giccwo EtKansaoo',
+          publishedDate: 'Apr 25, 2020',
+          level: 'Beginner',
+          numOfRates: 5.0,
+          score: 2101,
+          courseLength: '11h10m',
+        },
+        {
+          id: 4,
+          courseName: 'Design pattern, Javascript',
+          isPopular: false,
+          isNew: false,
+          authorName: 'Tom Ford',
+          publishedDate: 'Sep 20, 2019',
+          level: 'Middle',
+          numOfRates: 4.5,
+          score: 1102,
+          courseLength: '12h2m',
+        },
+        {
+          id: 5,
+          courseName: 'My SQL, Relationship Database RSMDC',
+          isPopular: true,
+          isNew: true,
+          authorName: 'Makr Kizamic',
+          publishedDate: 'Apr 20, 2020',
+          level: 'Advance',
+          numOfRates: 4.5,
+          score: 2992,
+          courseLength: '8h20m',
+        },
+      ],
+    }
+  },
 }
 </script>
 
@@ -28,5 +100,8 @@ export default {
 }
 .home-course-list > .sumary > .sortby {
   @apply text-gray-400;
+}
+.home-course-list > .items > .item {
+  @apply py-6 border-t border-gray-700;
 }
 </style>

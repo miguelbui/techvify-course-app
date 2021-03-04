2d0b916ae71b82d83eff5ca3bf15ca13ec8e0de7
miguelbui <miguelbui.dev@gmail.com>
2021-03-05 02:52:44 +0700

    :+1: Implement filter component

---
+++ components/pages/home/filter-course.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="filter-courses">
+    <button class="button">
+      <icon-filter class="icon"></icon-filter>
+      <span class="text">Filter</span>
+    </button>
+    <common-c-dropdown
+      v-model="filter.type"
+      class="w-64"
+      :items="filterType"
+      @change="updateTypeFilter"
+    ></common-c-dropdown>
+    <button class="button" @click="resetFilter">
+      <icon-clear class="icon"></icon-clear>
+      <span class="text">Clear all</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import { filterType } from '@/utils/constant'
+import { mapActions } from 'vuex'
+export default {
+  data() {
+    return {
+      filter: {
+        type: null,
+      },
+      filterType,
+    }
+  },
+  methods: {
+    ...mapActions({ setFilter: 'course/setFilter' }),
+    updateTypeFilter(payload) {
+      const filterItem = {
+        props: 'type',
+        payload,
+      }
+      this.setFilter(filterItem)
+    },
+    resetFilter() {
+      this.filter.type = null
+      this.updateTypeFilter(null)
+    },
+  },
+}
+</script>
+
+<style lang="postcss">
+.filter-courses {
+  @apply flex flex-row justify-start items-center text-sm space-x-1;
+}
+.filter-courses > .button {
+  @apply flex flex-row justify-start items-center px-4 py-2 rounded-sm bg-gray-600 text-gray-200 space-x-1;
+}
+.filter-courses > .button > .icon {
+  @apply w-5 h-5;
+}
+</style>
---
+++ utils/constant.js
@@ -0,0 +1,10 @@
+export const filterType = [
+  {
+    value: 'POPULARITY',
+    text: 'Popularity',
+  },
+  {
+    value: 'NEWEST',
+    text: 'Newest',
+  },
+]
